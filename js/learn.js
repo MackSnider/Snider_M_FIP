b6a98d32c9c5165615c0ec5d1604a1f5d81a3028
Mackenzie Snider <macksnider@gmail.com>
2021-04-08 23:47:53 -0700

    ending for the night

--- js/learn.js
+++ js/learn.js
@@ -3,3 +3,21 @@ $(document).ready(function(){
 		$("#reveal").slideToggle(300);
 	});
 });
+
+$(document).ready(function(){
+	$("#ads").click(function(){
+		$("#dropDown").slideToggle(300);
+	});
+});
+
+$(document).ready(function(){
+	$("#ad2").click(function(){
+		$("#dropDown2").slideToggle(300);
+	});
+});
+
+$(document).ready(function(){
+	$("#ad3").click(function(){
+		$("#dropDown3").slideToggle(300);
+	});
+});
